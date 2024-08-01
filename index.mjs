6d773e09a639768c7d76fd49f5db62555bb904a4
Mark Rodgers <markrodgers.dev@gmail.com>
2024-08-01 16:55:58 +0100

    Refactor and clean code for readability

--- index.mjs
+++ index.mjs
@@ -15,42 +15,43 @@ const knight = {
   ],
 };
 
-function knightMoves(startPos, endPos) {
-  // knightMoves([0,0], [3,3])
-  // returns array of the path co-ords, start and end inclusive, [[0,0], [1,2], [3,3]]
-  // sometimes there will be more than 1 solution, all are correct as long as it has the same least amount of moves
-  const adjacencyList = [];
-  const queue = [];
-}
-
+// Takes a position on the board and returns all valid accessible knight moves
 function getValidMoves(position) {
-  const xPos = position[0];
-  const yPos = position[1];
+  const [xPos, yPos] = position;
   const validMoves = [];
 
   knight.moves.forEach((move) => {
+    const [xKnightMove, yKnightMove] = move;
     if (
-      xPos + move[0] >= 0 &&
-      xPos + move[0] <= 7 &&
-      yPos + move[1] >= 0 &&
-      yPos + move[1] <= 7
+      xPos + xKnightMove >= 0 &&
+      xPos + xKnightMove < 8 &&
+      yPos + yKnightMove >= 0 &&
+      yPos + yKnightMove < 8
     ) {
-      validMoves.push([xPos + move[0], yPos + move[1]]);
+      validMoves.push([xPos + xKnightMove, yPos + yKnightMove]);
     }
   });
   return validMoves;
 }
 
 // 64 item array(0-63 indexed) to represent the 8x8 board as a graph(adjacency list)
-// Access desired x,y co-ordinates via graph[x * 7 + y] because of 0 indexing
+// Access desired x,y co-ordinates via graph[x + (y * 8)] because of 0 indexing a 1d array
 function buildGraph() {
   const graph = [];
-  for (let i = 0; i < 7; i++) {
-    for (let j = 0; j < 7; j++) {
-      let row = i;
-      let col = j;
-      graph.push(getValidMoves([row, col]));
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      let xPos = j;
+      let yPos = i;
+      graph.push(getValidMoves([xPos, yPos]));
     }
   }
   return graph;
+}
+
+// knightMoves([0,0], [3,3])
+// Returns array of the path co-ords, start and end inclusive, [[0,0], [1,2], [3,3]]
+// Sometimes there will be more than 1 solution, all are correct as long as it has the same least amount of moves
+function knightMoves(startPos, endPos) {
+  const graph = buildGraph();
+  const queue = [];
 }
